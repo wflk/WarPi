3f4d68c0f93475ffbf1e82113e45030032381a07
Trig0n <pascal@eberlein.io>
2016-08-04 11:24:13 +0200

    various database functions, such as ap to db // location to db // gp in db

--- Database/Database.cpp
+++ Database/Database.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "Database.h"
+#include <json/json.h>
+#include <restclient-cpp/restclient.h>
+#include <ctime>
+
+bool Database::gps_logging(double longitude, double latitude, double speed) {
+    std::time_t ts = std::time(nullptr);
+    Json::Value data;
+    data[std::to_string(ts)]["longitude"] = longitude;
+    data[std::to_string(ts)]["latitude"] = latitude;
+    data[std::to_string(ts)]["speed"] = speed;
+    RestClient::Response r;
+
+}
+
+bool Database::ap_in_db(std::string essid, std::string bssid) {
+    return true;
+}
+
+bool Database::ap_logging(double longitude, double latitude, double speed, std::string essid, std::string bssid,
+                          double freq, std::string encryption) {
+    return true;
+}
--- Database/Database.h
+++ Database/Database.h
@@ -6,7 +6,28 @@
 #define WARPI_DATABASE_H
 
 
+#include <string>
+#include "../Client/Client.h"
+#include "../Eve/Eve.h"
+#include <boost/thread/thread.hpp>
+
 class Database {
+private:
+    Eve eve;
+    std::string database_port = "";
+    boost::thread *eve_thread = new boost::thread(&Eve::run);
+public:
+    bool gps_logging(double longitude, double latitude, double speed);
+
+    bool ap_logging(double longitude, double latitude, double speed, std::string essid, std::string bssid, double freq,
+                    std::string encryption);
+
+    bool ap_in_db(std::string essid, std::string bssid);
+
+
+
+
+
 
 };
 
