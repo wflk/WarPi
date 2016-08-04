f7292cc1fe22ee0971e92e1048c70e31d44ef060
Trig0n <pascal@eberlein.io>
2016-08-04 09:41:26 +0200

    apparently isnan is in std

--- GPS/GPS.cpp
+++ GPS/GPS.cpp
@@ -23,7 +23,7 @@ gps_data_t GPS::get_current_location() {
             return gps_data;
         } else {
             if ((gps_data.status == STATUS_FIX) && (gps_data.fix.mode == MODE_2D || gps_data.fix.mode == MODE_3D) &&
-                !isnan(gps_data.fix.longitude) && !isnan(gps_data.fix.latitude)) {
+                !std::isnan(gps_data.fix.longitude) && !std::isnan(gps_data.fix.latitude)) {
                 return gps_data;
             } else {
                 this->last_error == "NO_GPS_DATA";
